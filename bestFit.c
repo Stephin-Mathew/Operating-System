a10544cb387fb29ce375ffab74f41bd1d0c94ae4
Stephin Mathew <109020237+Stephin-Mathew@users.noreply.github.com>
2024-04-19 12:39:06 +0530

    Add files via upload

---
+++ bestFit.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+void main(){
+int lim,mem;
+ struct mems{
+  int size,alloc;
+  }M[10];
+  
+ struct Proc{
+  int size,flag;
+  }P[10];
+
+
+printf("Number of Processes : \n");
+scanf("%d",&lim);
+printf("Number of Memory Blocks : \n");
+scanf("%d",&mem);
+for(int i=0;i<mem;i++){
+ printf("Enter Memory Size :\n");
+ scanf("%d",&M[i].size);
+ M[i].alloc=0;
+}
+for(int i=0;i<mem;i++){
+ for(int j=i+1;j<mem;j++){
+  if(M[i].size>=M[j].size){
+   int temp=M[i].size;
+   M[i].size=M[j].size;
+   M[j].size=temp;
+} 
+}
+}
+for(int i=0;i<lim;i++){
+ printf("Enter Process Size :\n");
+ scanf("%d",&P[i].size);
+ P[i].flag=0;
+}
+
+for(int i=0;i<lim;i++){
+ for(int j=0;j<mem;j++){
+  if(P[i].size<=M[j].size){
+   if(M[j].alloc==1)
+     continue;
+   else{
+    M[j].alloc=1;
+    P[i].flag=1;
+     printf("%d Memory is allocated for %d Process \n",M[j].size,P[i].size);
+     break;
+    }
+   }
+  }
+ }
+
+for(int i=0;i<lim;i++){
+  if(P[i].flag==0){
+   printf("No Space for %d process \n",P[i].size);
+  }
+ } 
+
+}
